038f4e627064ddc896a4fb42bc242c6f8536a477
anton <a.solovjev@vzv.su>
2023-11-08 12:16:57 +0300

    Extend data validation for def register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,3 +1,4 @@
+import phonenumbers
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework import status
@@ -69,23 +70,28 @@ def register_order(request):
         content = {'error': f'Не переданы поля {checking_fields}'}
         return Response(content, status=status.HTTP_200_OK)
 
-    if not isinstance(order['address'], str):
-        content = {'error': 'Поле address должно быть строкой'}
+    if not isinstance(order['address'], str) or order['address'] == '':
+        content = {'error': 'Поле address должно быть не пустой строкой'}
         return Response(content, status=status.HTTP_200_OK)
 
-    if not isinstance(order['phonenumber'], str):
-        content = {'error': 'Поле phonenumber должно быть строкой'}
+    if not isinstance(order['phonenumber'], str) or order['phonenumber'] == '':
+        content = {'error': 'Поле phonenumber должно быть не пустой строкой'}
         return Response(content, status=status.HTTP_200_OK)
 
-    if not isinstance(order['lastname'], str):
-        content = {'error': 'Поле lastname должно быть строкой'}
+    phone_number = phonenumbers.parse(order['phonenumber'], "RU")
+    if not phonenumbers.is_valid_number(phone_number):
+        content = {'error': 'В поле phonenumber не валидный номер телефона'}
         return Response(content, status=status.HTTP_200_OK)
 
-    if not isinstance(order['firstname'], str):
-        content = {'error': 'Поле firstname должно быть строкой'}
+    if not isinstance(order['lastname'], str) or order['lastname'] == '':
+        content = {'error': 'Поле lastname должно быть не пустой строкой'}
         return Response(content, status=status.HTTP_200_OK)
 
-    if not isinstance(order['products'], list):
+    if not isinstance(order['firstname'], str) or order['firstname'] == '':
+        content = {'error': 'Поле firstname должно быть не пустой строкой'}
+        return Response(content, status=status.HTTP_200_OK)
+
+    if not isinstance(order['products'], list) or order['products'] == '':
         content = {'error': 'В products должен быть список'}
         return Response(content, status=status.HTTP_200_OK)
 
@@ -93,6 +99,25 @@ def register_order(request):
         content = {'error': 'Список products не может быть пустым'}
         return Response(content, status=status.HTTP_200_OK)
 
+    check_products = []
+    for product in order['products']:
+        if not isinstance(product['product'], int):
+            check_products.append(product['product'])
+        if not isinstance(product['quantity'], int):
+            check_products.append(product['quantity'])
+    if check_products:
+        content = {'error': f'Поля должны быть целыми числами {check_products}'}
+        return Response(content, status=status.HTTP_200_OK)
+
+    products_ids = [product['product'] for product in order['products']]
+    instance_products = {obj.id: obj for obj in Product.objects.filter(id__in=products_ids)}
+    if not instance_products:
+        content = {'error': f'Не найдены записи в бд с id {products_ids}'}
+        return Response(content, status=status.HTTP_200_OK)
+    if len(instance_products) != len(products_ids):
+        not_in_id = [i for i in instance_products if i not in products_ids]
+        content = {'error': f'Не найдены записи в бд с id {not_in_id}'}
+        return Response(content, status=status.HTTP_200_OK)
 
     instance_order = Order.objects.create(
         firstname=order['firstname'],
@@ -100,9 +125,6 @@ def register_order(request):
         phone_number=order['phonenumber'],
         address=order['address']
     )
-
-    products_ids = [product['product'] for product in order['products']]
-    instance_products = {obj.id: obj for obj in Product.objects.filter(id__in=products_ids)}
 
     for product in order['products']:
         OrderItem.objects.create(
